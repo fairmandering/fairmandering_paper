45f8a7aa013b707a20fc922b3b4ac0b0812e64fa
Wes Gurnee <rwg97@cornell.edu>
2020-08-22 11:56:05 -0400

    .csv

--- gerrypy/utils/dir_processing.py
+++ gerrypy/utils/dir_processing.py
@@ -17,9 +17,10 @@ def district_df_of_tree_dir(dir_path):
         block_district_matrix = make_bdm(tree_data['leaf_nodes'], len(state_df))
         district_df = create_district_df(block_district_matrix, state_df)
         
-        district_df.to_csv(os.path.join(dir_path, state_abbrev))
+        district_df.to_csv(os.path.join(dir_path, state_abbrev + '.csv'))
         elapsed_t = str(round((time.time() - start_t) / 60, 2))
         print('Built and saved', state_abbrev, 'in', elapsed_t, 'mins')
+
         
 if __name__ == '__main__':
     paths_to_process = [
