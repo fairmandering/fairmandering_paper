2a685e7fad8368ce7f1dc96583a4589b8531a226
Wes Gurnee <rwg97@cornell.edu>
2020-09-26 14:51:26 -0400

    election_df script

---
+++ gerrypy/pipelines/create_election_dfs.py
@@ -0,0 +1,9 @@
+import os
+from gerrypy import constants
+from gerrypy.data.precinct_state_wrappers import wrappers
+
+for state, wrapper in wrappers.items():
+    print(state)
+    election_df = wrapper().get_data()
+    save_path = os.path.join(constants.OPT_DATA_PATH, state, 'election_df.csv')
+    election_df.to_csv(save_path, index=False)
