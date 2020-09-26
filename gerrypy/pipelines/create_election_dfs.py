9bf70468d9427160c2d7e3d4e59f993ffbe07a70
Wes Gurnee <rwg97@cornell.edu>
2020-09-26 15:04:26 -0400

    Catch not implemented errors in creating election_dfs

--- gerrypy/pipelines/create_election_dfs.py
+++ gerrypy/pipelines/create_election_dfs.py
@@ -4,6 +4,9 @@ from gerrypy.data.precinct_state_wrappers import wrappers
 
 for state, wrapper in wrappers.items():
     print(state)
-    election_df = wrapper().get_data()
-    save_path = os.path.join(constants.OPT_DATA_PATH, state, 'election_df.csv')
-    election_df.to_csv(save_path, index=False)
+    try:
+        election_df = wrapper().get_data()
+        save_path = os.path.join(constants.OPT_DATA_PATH, state, 'election_df.csv')
+        election_df.to_csv(save_path, index=False)
+    except NotImplementedError:
+        print('Not implemented')
